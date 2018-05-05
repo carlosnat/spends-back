0eaf94857f1072acb961c2f1d64a93cf3d1bbac1
Carlos <carlos@reigndesign.com>
2018-05-05 11:57:05 -0300

    adding files to operation

--- api/operation/operation.model.js
+++ api/operation/operation.model.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const operationSchema = mongoose.Schema({
-    _id: Schema.Types.ObjectId,
     family: [{ type: Schema.Types.ObjectId, ref: 'Family' }],
     category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
     amount: { type: Number },
     description: { type: String },
     occurrenceDate: { type: Date },
-    creationDate: { type: Date }
+    creationDate: { type: Date },
+    image: { type: Schema.Types.Mixed }
 });
 
 module.exports = mongoose.model('Operation', operationSchema);
--- api/operation/operation.routes.js
+++ api/operation/operation.routes.js
@@ -3,6 +3,32 @@
 var operationModel = require('./operation.model');
 var routes = require('../routes');
 
+var cloudinary = require('cloudinary');
+var cloudinaryStorage = require('multer-storage-cloudinary');
+var multer = require('multer');
+
+cloudinary.config({ 
+    cloud_name: 'supercars', 
+    api_key: '163645418898372', 
+    api_secret: 'YE5xe0x5IGKL7bdUebAHvS74aU8' 
+});
+
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: 'folder-name',
+    allowedFormats: ['jpg', 'png'],
+    filename: function (req, file, cb) {
+        cb(undefined, 'my-file-name');
+    }
+});
+
+const parser = multer({ storage: storage });
+
 module.exports = function(app){
+
+    app.post('/api/operation/upload', parser.single('image'), function (req, res) {
+        res.json(req.file);
+    });
+
     routes(app, operationModel, 'operation');
 }
