df9e1926bd16f3ee58d2035bbd75976e797cd15d
Carlos <carlos@reigndesign.com>
2018-05-05 13:53:18 -0300

    configurando variables de entorno

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/family-spend');
-// mongoose.connect("mongodb://cars:DTEbjBDR3Od6Iim5@cluster0-shard-00-00-zdfxp.mongodb.net:27017,cluster0-shard-00-01-zdfxp.mongodb.net:27017,cluster0-shard-00-02-zdfxp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
+mongoose.connect(process.env.DATABASE);
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
