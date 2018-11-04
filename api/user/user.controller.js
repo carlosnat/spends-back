8c926f64abd5ad4d5f512b9f0429992eb695a752
Carlos <natearcarlos@gmail.com>
2018-11-04 14:07:39 -0300

    fix bug on signup

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -6,15 +6,16 @@ exports.signup = async (req, res) => {
   const checkEmail = await User.find({ email: req.body.email }).lean();
   if (checkEmail.length > 0) {
     res.status(500).json({ error_code: 10, error_msg: 'Email user already existed' });
-  }
-  try {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    req.body.createdAt = Date.now();
-    req.body.password = hash;
-    const userCreated = await User.create(req.body);
-    res.json({ userCreated });
-  } catch (error) {
-    res.status(500).send({ error: JSON.parse(JSON.stringify(error, Object.getOwnPropertyNames(error))) });
+  } else {
+    try {
+      const hash = await bcrypt.hash(req.body.password, 10);
+      req.body.createdAt = Date.now();
+      req.body.password = hash;
+      const userCreated = await User.create(req.body);
+      res.json({ userCreated });
+    } catch (error) {
+      res.status(500).send({ error: JSON.parse(JSON.stringify(error, Object.getOwnPropertyNames(error))) });
+    }
   }
 };
 
