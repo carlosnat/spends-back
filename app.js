c17cd4a1b5e0c7f02092032076d9a0b3d42a09e1
Carlos Natera <carlos@reigndesign.com>
2018-12-17 17:19:46 -0300

    cleaning app file

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ app.get('/api/health', async (req, res) => {
   res.json({ msg: 'welcome to family-spend-api' });
 });
 
-/* require('./api/family/family.routes')(app);
-require('./api/category/category.routes')(app);
-require('./api/operation/operation.routes')(app);
-require('./api/spendGroup/spendGroup.routes')(app); */
-
 app.use(errorMiddleware);
 
 module.exports = app;
